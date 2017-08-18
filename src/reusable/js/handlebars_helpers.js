30548887e91b0e6ec33664af739662fd37dfd934
Arthur Shpakov <arthur@amplience.com>
2017-08-18 13:38:23 +0300

    several enahncements and build

--- src/reusable/js/handlebars_helpers.js
+++ src/reusable/js/handlebars_helpers.js
@@ -37,18 +37,18 @@ if (typeof Handlebars !== 'undefined') {
     });
 
     Handlebars.registerHelper('bannerConfig',function (opts) {
-       hex = this.bannerColor;
-       alpha = this.bannerOpacity;
-       hex   = hex.replace('#', '');
-       var r = parseInt(hex.length == 3 ? hex.slice(0, 1).repeat(2) : hex.slice(0, 2), 16);
-       var g = parseInt(hex.length == 3 ? hex.slice(1, 2).repeat(2) : hex.slice(2, 4), 16);
-       var b = parseInt(hex.length == 3 ? hex.slice(2, 3).repeat(2) : hex.slice(4, 6), 16);
-       if ( alpha ) {
-          return 'background-color:rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + '); ';
-       }
-       else {
-          return 'background-color:rgb(' + r + ', ' + g + ', ' + b + '); ';
-      }
+        hex = this.bannerColor || '#fff';
+        alpha = this.bannerOpacity || 1;
+        hex   = hex.replace('#', '');
+        var r = parseInt(hex.length == 3 ? hex.slice(0, 1).repeat(2) : hex.slice(0, 2), 16);
+        var g = parseInt(hex.length == 3 ? hex.slice(1, 2).repeat(2) : hex.slice(2, 4), 16);
+        var b = parseInt(hex.length == 3 ? hex.slice(2, 3).repeat(2) : hex.slice(4, 6), 16);
+        if ( alpha ) {
+            return 'background-color:rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + '); ';
+        }
+        else {
+            return 'background-color:rgb(' + r + ', ' + g + ', ' + b + '); ';
+        }
     });
 
     Handlebars.registerHelper('roundelConfig', function (roundel) {
